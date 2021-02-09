398108fd96d1214f6c6de28ee523097f3b601cb5
Bun <Bun@DESKTOP-HAH8AM1.fios-router.home>
2021-02-09 09:55:56 -0500

    Commit 2 - 25-->50

--- Snakey/src/GamePanel.java
+++ Snakey/src/GamePanel.java
@@ -16,7 +16,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
 	static final int SCREEN_WIDTH = 600;
 	static final int SCREEN_HEIGHT = 600;
-	static final int UNIT_SIZE = 25;
+	static final int UNIT_SIZE = 50;
 	static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
 	static final int DELAY = 75;
 	final int x[] = new int[GAME_UNITS]; // hold coordinates of body parts
